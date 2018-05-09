f370ac4c614861e32b567b0833febaaeb65ecc9a
6Andrea <andreaamezcua@student.hartnell.edu>
2018-05-09 15:47:58 -0700

    update

--- gui.py
+++ gui.py
@@ -1,35 +1,88 @@
+# CST-205 Multimedia Design & Programming
+#
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QFileDialog
+import cv2
+from PyQt5 import QtCore
+from PyQt5.QtGui import QImage, QPixmap
+from google_images_download import google_images_download
+from PIL import Image
+import glob
+from keyword_search import keyword
 
 class gui(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('205 Project')
-        self.label = QLabel('Message: ')
+        self.btn1 = QPushButton('Decode')
+        self.btn2 = QPushButton('Encode')
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.btn2)
+        hbox.addWidget(self.btn1)
+        self.setLayout(hbox)
+
+        self.btn2.clicked.connect(self.encode)
+        self.btn1.clicked.connect(self.decode)
+    @pyqtSlot()
+    def encode(self):
+        self.new_win = EncodeWindow()
+        self.new_win.show()
+
+    def decode(self):
+        self.new_win = DecodeWindow()
+        self.new_win.show()
+
+class EncodeWindow(QWidget):
+    def __init__(self):
+        super().__init__()
         self.line_edit = QLineEdit()
         self.line_edit.selectAll()
         self.line_edit.setFocus()
         self.btn = QPushButton('Search')
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.label)
         vbox.addWidget(self.line_edit)
         vbox.addWidget(self.btn)
         self.setLayout(vbox)
 
-#        self.btn.clicked.connect(self.open_win)
-'''
-        @pyqtSlot()
-        def open_win(self):
-            choice = QtGui.QMessageBox.question(self, "Hey", "ok")
+        self.btn.clicked.connect(self.getText)
 
-            text = self.line_edit.currentText()
-            self.
-            self.new_win.show()
+    @pyqtSlot()
+    def getText(self):
+        text = self.line_edit.text()
+        #print(text)
+        keyword(text)
 
-'''
+class DecodeWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.image=None
+        self.btn0 = QPushButton('Get Image')
+        self.boton = QPushButton('Get Message')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.btn0)
+        vbox.addWidget(self.boton)
+        self.setLayout(vbox)
+
+        self.btn0.clicked.connect(self.getImage)
+        #self.button.clicked.connect(self.)
+
+    @pyqtSlot()
+    def getImage(self):
+        fname, filter = QFileDialog.getOpenFileName(self, 'Open File', 'c:\\', "Image File (*.jpeg)")
+        if fname:
+            self.loadImage(fname)
+        else:
+            print('Invalid Image')
+
+    def loadImage(self, fname):
+        self.image = cv2.imread(fname)
 
 app = QApplication(sys.argv)
 main_win = gui()
--- keyword_search.py
+++ keyword_search.py
@@ -1,36 +1,34 @@
 from google_images_download import google_images_download
 from PIL import Image
 import glob
-unwanted_words = [", ",",","!","?",".","name","one","ones","had","hid","inside","outside","then","only","was","such","pounded","pounding","gradually","above","below","but","butt","thin","fat","at","first","second","third","fourth","fifth","sixth","all","wasnt","wasn't","than","agree","disagree","changed","change","sit","sits","sitting","unchanged","probably","actually","excluded","included","anyone","more","feel","need","likely","needs","feels","feeling","friend","that","will","many","got","into","out","up","down","fell","whom","are","aren't","arent","fall","never","left","right","diagonal","one","knife","stab","shoot","shot","i'll","know","now","an","going","like","so","how","wouldnt","would","shouldnt","of","alot","be","because","been","as","shouldn't","can't","cant","can","cannot","isnt","why","who","what","when","were","where","yeah","your","you","too","two","to","hello","my","the","in","and","a","i","the","or","is","not","this","on","for","from","them","his","her","if","have","havent","haven’t","they","it","their","there","they're","isn't","he","she","we","should","could","couldn't"]
-def keywords(string):
-    key_words = []
-    words = string.split()
-    for i in words:
-        if i[-1] == '.' or i[-1] == '!' or i[-1] == '?' or i[-1] == ',' or i[-1] == ';':
-            i = i[0:-1]
-        if i.lower() not in unwanted_words and i.lower() not in key_words:
-            key_words.append(i.lower())
-    return key_words
 
-
-new_string = "order66"
-new_list = keywords(new_string)
-other_string = ""
-print(new_list)
-for item in new_list:
-    if item != new_list[-1]:
-        other_string += (item + ',')
-    else:
-        other_string += item
-print(other_string)
-resp = google_images_download.googleimagesdownload()
-
-arguments = {"keywords":other_string,"limit":1,"print_urls":True,"format":"jpg"}
-resp.download(arguments)
-
-for word in new_list:
-    image_file = glob.glob('./downloads/'+ word +'/*.jpg')
-    print(image_file)
-    for i in range(len(image_file)):
-        image_file[i] = Image.open(image_file[i])
-        image_file[i].show()
+def keyword(string):
+    unwanted_words = [", ",",","!","?",".","name","one","ones","had","hid","inside","outside","then","only","was","such","pounded","pounding","gradually","above","below","but","butt","thin","fat","at","first","second","third","fourth","fifth","sixth","all","wasnt","wasn't","than","agree","disagree","changed","change","sit","sits","sitting","unchanged","probably","actually","excluded","included","anyone","more","feel","need","likely","needs","feels","feeling","friend","that","will","many","got","into","out","up","down","fell","whom","are","aren't","arent","fall","never","left","right","diagonal","one","knife","stab","shoot","shot","i'll","know","now","an","going","like","so","how","wouldnt","would","shouldnt","of","alot","be","because","been","as","shouldn't","can't","cant","can","cannot","isnt","why","who","what","when","were","where","yeah","your","you","too","two","to","hello","my","the","in","and","a","i","the","or","is","not","this","on","for","from","them","his","her","if","have","havent","haven’t","they","it","their","there","they're","isn't","he","she","we","should","could","couldn't"]
+    def keywords(string):
+        key_words = []
+        words = string.split()
+        for i in words:
+            if i[-1] == '.' or i[-1] == '!' or i[-1] == '?' or i[-1] == ',' or i[-1] == ';':
+                i = i[0:-1]
+            if i.lower() not in unwanted_words and i.lower() not in key_words:
+                key_words.append(i.lower())
+        return key_words
+    new_string = string
+    new_list = keywords(new_string)
+    other_string = ""
+    #print(new_list)
+    for item in new_list:
+        if item != new_list[-1]:
+            other_string += (item + ',')
+        else:
+            other_string += item
+            #print(other_string)
+            resp = google_images_download.googleimagesdownload()
+            arguments = {"keywords":other_string,"limit":1,"print_urls":True,"format":"jpg"}
+            resp.download(arguments)
+            for word in new_list:
+                image_file = glob.glob('./downloads/'+ word +'/*.jpg')
+                print(image_file)
+                for i in range(len(image_file)):
+                    image_file[i] = Image.open(image_file[i])
+                    image_file[i].show()
