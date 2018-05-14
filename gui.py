d19c27d3ae48c3be1e5a00df232517c4421efb62
6Andrea <andreaamezcua@student.hartnell.edu>
2018-05-13 18:18:56 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 # CST-205 Multimedia Design & Programming
-# Authors: Blayne Suttinwills, Jacob Fahy, Andrea Amezcua Moreno
+# Authors: Blayne Suttonwills, Jacob Fahy, Andrea Amezcua Moreno
 # May 14, 2018
+# https://github.com/jakefahy/205project
+# The code takes a message and encodes it into an image. The image represents the message. Then, with the
+# decoding button, the the code returns the secret message stored in the image.
+
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
 from PyQt5.QtCore import pyqtSlot
@@ -12,15 +16,17 @@ from google_images_download import google_images_download
 from PIL import Image
 import glob
 from keyword_search import keyword
+from lsb_steganography import decoding
+#Andrea Amezcua worked on this file
 
 class gui(QWidget):
     def __init__(self):
         super().__init__()
-
+        #make buttons
         self.setWindowTitle('205 Project')
         self.btn1 = QPushButton('Decode')
         self.btn2 = QPushButton('Encode')
-
+        #layout the buttons
         hbox = QHBoxLayout()
         hbox.addWidget(self.btn2)
         hbox.addWidget(self.btn1)
@@ -34,9 +40,8 @@ class gui(QWidget):
         self.new_win.show()
 
     def decode(self):
-        self.new_win = DecodeWindow()
-        self.new_win.show()
-
+        decoding()
+#Encoding the message into the image
 class EncodeWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -51,39 +56,13 @@ class EncodeWindow(QWidget):
         self.setLayout(vbox)
 
         self.btn.clicked.connect(self.getText)
-
+#Decoding the image and getting the message back
     @pyqtSlot()
     def getText(self):
         text = self.line_edit.text()
         #print(text)
         keyword(text)
 
-class DecodeWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.image=None
-        self.btn0 = QPushButton('Get Image')
-        self.boton = QPushButton('Get Message')
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.btn0)
-        vbox.addWidget(self.boton)
-        self.setLayout(vbox)
-
-        self.btn0.clicked.connect(self.getImage)
-        #self.button.clicked.connect(self.)
-
-    @pyqtSlot()
-    def getImage(self):
-        fname, filter = QFileDialog.getOpenFileName(self, 'Open File', 'c:\\', "Image File (*.jpeg)")
-        if fname:
-            self.loadImage(fname)
-        else:
-            print('Invalid Image')
-
-    def loadImage(self, fname):
-        self.image = cv2.imread(fname)
 
 app = QApplication(sys.argv)
 main_win = gui()
